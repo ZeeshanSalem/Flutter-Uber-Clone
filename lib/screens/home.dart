a3546949a8c037e787e014744bb1a3618e0ccfa4
ZeeshanSalem <zeeshansaleem729@gmail.com>
2020-11-30 22:47:21 +0500

    ADD DEPENDENCE FOR ANDROID

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -147,7 +147,7 @@ class _MapState extends State<Map> {
     });
   }
 
-  void _onAddMarkerPress() {
+  void onAddMarkerPress() {
     setState(() {
       _markers.add(Marker(markerId: MarkerId(_lastPosition.toString()),
           position: _lastPosition,
@@ -159,4 +159,40 @@ class _MapState extends State<Map> {
       ));
     });
   }
+
+  List decodePoly(String poly) {
+    var list = poly.codeUnits;
+    var lList = new List();
+    int index = 0;
+    int len = poly.length;
+    int c = 0;
+// repeating until all attributes are decoded
+    do {
+      var shift = 0;
+      int result = 0;
+
+      // for decoding value of one attribute
+      do {
+        c = list[index] - 63;
+        result |= (c & 0x1F) << (shift * 5);
+        index++;
+        shift++;
+      } while (c >= 32);
+      /* if value is negetive then bitwise not the value */
+      if (result & 1 == 1) {
+        result = ~result;
+      }
+      var result1 = (result >> 1) * 0.00001;
+      lList.add(result1);
+    } while (index < len);
+
+/*adding to previous value as done in encoding */
+    for (var i = 2; i < lList.length; i++) lList[i] += lList[i - 2];
+
+    print(lList.toString());
+
+    return lList;
+  }
+
+
 }
